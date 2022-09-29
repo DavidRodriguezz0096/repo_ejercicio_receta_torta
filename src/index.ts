25613925b677bbda20d34c8db5deb942157355ed
DavidRodriguezz0096 <110864324+DavidRodriguezz0096@users.noreply.github.com>
2022-09-29 11:10:04 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+// Filmina 1: Receta torta - Alumno: David Rodriguez
+// Ingredientes: 6 huevos, 200gr de harina leudante, 1 gota de esencia de vainilla.
+let pasoUno = "Batir los huevos con el azucar, hasta duplicar su volumen";
+let pasoDos = "Incorporar harina con movimientos envolventes";
+let pasoTres = "Agregar esencia de vainilla";
+let pasoCuatro = "Colocar en placa enmantecada";
+let pasoCinco = "Colocar placa en el horno";
+let pasoSeis = "Dejar la placa durante 45 o 50 minutos";
+let pasoSiete = "Transcurrido el tiempo, retirar la placa";
+let pasoOcho = "Desmoldar y dejar enfriar";
+let pasoNueve = "Decorar a gusto";
+let pasoDiez = "Preparar infusión preferida y a disfrutar!!!";
+
+console.log(
+  pasoUno,
+  pasoDos,
+  pasoTres,
+  pasoCuatro,
+  pasoCinco,
+  pasoSeis,
+  pasoSiete,
+  pasoOcho,
+  pasoNueve,
+  pasoDiez
+);
